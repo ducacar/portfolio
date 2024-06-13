a034dbf813f4ae9bea2fc7e254d35c73c19ed4c4
Dusan <ducagardinovacki3@gmail.com>
2024-06-13 13:36:24 +0200

    added content 2

--- public/js/javascript.js
+++ public/js/javascript.js
@@ -1,20 +1,20 @@
 // JavaScript code for expand buttons and modals
-const expandButtons = document.querySelectorAll('.expand-button');
-const modals = document.querySelectorAll('.modal');
-const closeButtons = document.querySelectorAll('.modal-close');
+const expandButtons = document.querySelectorAll(".expand-button");
+const modals = document.querySelectorAll(".modal");
+const closeButtons = document.querySelectorAll(".modal-close");
 
 expandButtons.forEach((button) => {
-  button.addEventListener('click', () => {
-    const modalId = button.getAttribute('data-modal-id');
+  button.addEventListener("click", () => {
+    const modalId = button.getAttribute("data-modal-id");
     const modal = document.getElementById(modalId);
-    modal.style.display = 'block';
+    modal.style.display = "block";
   });
 });
 
 closeButtons.forEach((button) => {
-  button.addEventListener('click', () => {
-    const modal = button.closest('.modal');
-    modal.style.display = 'none';
+  button.addEventListener("click", () => {
+    const modal = button.closest(".modal");
+    modal.style.display = "none";
   });
 });
 
@@ -27,12 +27,12 @@ const progress_bars = document.querySelectorAll(".skills svg circle");
 
 const links = document.querySelectorAll(".nav-link");
 
-const hamburger = document.querySelector(".hamburger")
+const hamburger = document.querySelector(".hamburger");
 
 window.addEventListener("scroll", () => {
   activeLink();
-  if(!skillsPlayed) skillsCounter();
-})
+  if (!skillsPlayed) skillsCounter();
+});
 
 function stickyNavbar() {
   header.classList.toggle("scrolled", window.pageYOffset > 0);
@@ -45,51 +45,51 @@ window.addEventListener("scroll", stickyNavbar);
 // JavaScript code for animations
 const sr = ScrollReveal({
   duration: 2500,
-  distance: "60px"
+  distance: "60px",
 });
 
 sr.reveal(".showcase-info", { delay: 400 });
 sr.reveal(".showcase-image", { origin: "top", delay: 500 });
-sr.reveal(".website-grid", {origin:"left", delay: 400});
-sr.reveal(".contact-info", {origin:"left", delay: 400});
-sr.reveal(".contact-grid", {origin:"bottom", delay: 500});
+sr.reveal(".website-grid", { origin: "left", delay: 400 });
+sr.reveal(".contact-info", { origin: "left", delay: 400 });
+sr.reveal(".contact-grid", { origin: "bottom", delay: 500 });
 
 function hasReached(el) {
   let topPosition = el.getBoundingClientRect().top;
-  if(window.innerHeight >= topPosition + el.offsetHeight) return true;
+  if (window.innerHeight >= topPosition + el.offsetHeight) return true;
   return false;
 }
 
-function updateCount (num, maxNum) {
+function updateCount(num, maxNum) {
   let currentNum = +num.innerText;
-  if(currentNum < maxNum) {
+  if (currentNum < maxNum) {
     num.innerText = currentNum + 1;
     setTimeout(() => {
-      updateCount(num, maxNum)
+      updateCount(num, maxNum);
     }, 12);
   }
-};
+}
 
-let skillsPlayed = false
+let skillsPlayed = false;
 
-function skillsCounter(){
-  if(!hasReached(first_skill)) return;
-  
+function skillsCounter() {
+  if (!hasReached(first_skill)) return;
+
   skillsPlayed = true;
   sk_counters.forEach((counter, i) => {
     let target = +counter.dataset.target;
-    let strokeValue = 427 - 427 * (target/100);
+    let strokeValue = 427 - 427 * (target / 100);
 
-    progress_bars[i].style.setProperty("--target", strokeValue)
+    progress_bars[i].style.setProperty("--target", strokeValue);
 
     setTimeout(() => {
       updateCount(counter, target);
     }, 400);
-  })
+  });
 
-  progress_bars.forEach(p => p.style.animation = "progress 2s ease-in-out forwards");
-
-  
+  progress_bars.forEach(
+    (p) => (p.style.animation = "progress 2s ease-in-out forwards")
+  );
 }
 
 // JavaScript code for filtering cards
@@ -121,27 +121,30 @@ navButtons.forEach((button) => {
 
 function activeLink() {
   let sections = document.querySelectorAll("section[id]");
-  let passedSection = Array.from(sections).map((sct, i) => {
-    return {y: sct.getBoundingClientRect().top - header.offsetHeight,
-    id: i,
-    };
-  }).filter(sct => sct.y <= 0)
-  
+  let passedSection = Array.from(sections)
+    .map((sct, i) => {
+      return {
+        y: sct.getBoundingClientRect().top - header.offsetHeight,
+        id: i,
+      };
+    })
+    .filter((sct) => sct.y <= 0);
+
   let currSectionID = passedSection.at(-1).id;
-  links.forEach(l => l.classList.remove("active"));
-  links[currSectionID].classList.add("active")
+  links.forEach((l) => l.classList.remove("active"));
+  links[currSectionID].classList.add("active");
 }
 
 // Open and close nav menu
 
 hamburger.addEventListener("click", () => {
-  document.body.classList.toggle("open")
-  document.body.classList.toggle("stopScrolling")
-})
+  document.body.classList.toggle("open");
+  document.body.classList.toggle("stopScrolling");
+});
 
 links.forEach((link) => {
-  link.addEventListener("click", () =>{
-    document.body.classList.remove("open")
-  document.body.classList.remove("stopScrolling")
-  })
-})
+  link.addEventListener("click", () => {
+    document.body.classList.remove("open");
+    document.body.classList.remove("stopScrolling");
+  });
+});
